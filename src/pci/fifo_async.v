1aba422f74bb69818c3d706451cffadb521860c5
Johan Liu <liujiahan@gmail.com>
2016-05-07 20:32:27 +0800

    Add wr_count and rd_count support to fifo_async.v.

--- src/pci/fifo_async.v
+++ src/pci/fifo_async.v
@@ -1,22 +1,41 @@
 module fifo_async(
-	dout,
-	full,
-	empty,
+	wr_rst,
+	wr_clk,
 	din,
 	wr_en,
-	wr_clk,
-	wr_rst,
-	rd_en,
+	full,
+	wr_count,
+	rd_rst,
 	rd_clk,
-	rd_rst
+	dout,
+	rd_en,
+	empty,
+	rd_count
 );
 parameter DSIZE = 8;
 parameter ASIZE = 4;
 parameter MODE = "NORMAL";
 
+function [ASIZE:0] gray_encode(input [ASIZE:0] binary);
+begin
+	gray_encode = (binary>>1) ^ binary;
+end
+endfunction
+
+function [ASIZE:0] gray_decode(input [ASIZE:0] gray);
+	integer i;
+begin
+	gray_decode[ASIZE] = gray[ASIZE];
+	for(i=ASIZE-1;i>=0;i=i-1)
+		gray_decode[i] = gray_decode[i+1]^gray[i];
+end
+endfunction
+
 output [DSIZE-1:0] dout;
 output full;
 output empty;
+output [ASIZE:0] wr_count;
+output [ASIZE:0] rd_count;
 input [DSIZE-1:0] din;
 input wr_en, wr_clk, wr_rst;
 input rd_en, rd_clk, rd_rst;
@@ -26,6 +45,11 @@ reg [ASIZE:0] wptr, rptr;
 reg [ASIZE:0] rbin, wbin;
 (* ASYNC_REG = "TRUE" *) reg [ASIZE:0] wq2_rptr, wq1_rptr;
 (* ASYNC_REG = "TRUE" *) reg [ASIZE:0] rq2_wptr, rq1_wptr;
+reg [ASIZE:0] wq2_rbin;
+reg [ASIZE:0] rq2_wbin;
+reg [ASIZE:0] wr_count;
+reg [ASIZE:0] rd_count;
+
 reg [DSIZE-1:0] mem[0:(1<<ASIZE)-1];
 
 wire [ASIZE-1:0] waddr, raddr;
@@ -82,7 +106,8 @@ end
 // Memory read-address pointer (okay to use binary to address memory)
 assign raddr = rbin[ASIZE-1:0];
 assign rbinnext = rbin + (rd_en & ~empty);
-assign rgraynext = (rbinnext>>1) ^ rbinnext;
+//assign rgraynext = (rbinnext>>1) ^ rbinnext;
+assign rgraynext = gray_encode(rbinnext);
 
 // FIFO empty when the next rptr == synchronized wptr or on reset
 assign empty_val = (rgraynext == rq2_wptr);
@@ -92,6 +117,22 @@ begin
 	 empty <= 1'b1;
  else
 	 empty <= empty_val;
+end
+
+always @(posedge rd_clk or posedge rd_rst)
+begin
+	if(rd_rst)
+		rq2_wbin <= 0;
+	else
+		rq2_wbin <= gray_decode(rq2_wptr);
+end
+
+always @(posedge rd_clk or posedge rd_rst)
+begin
+	if(rd_rst)
+		rd_count <= 0;
+	else
+		rd_count <= rq2_wbin - rbinnext;
 end
 
 // Write pointers
@@ -106,7 +147,8 @@ end
 // Memory write-address pointer (okay to use binary to address memory)
 assign waddr = wbin[ASIZE-1:0];
 assign wbinnext = wbin + (wr_en & ~full);
-assign wgraynext = (wbinnext>>1) ^ wbinnext;
+//assign wgraynext = (wbinnext>>1) ^ wbinnext;
+assign wgraynext = gray_encode(wbinnext);
 
 //------------------------------------------------------------------
 // Simplified version of the three necessary full-tests:
@@ -122,6 +164,22 @@ begin
 		full <= 1'b0;
 	else
 		full <= full_val;
+end
+
+always @(posedge wr_clk or posedge wr_rst)
+begin
+	if(wr_rst)
+		wq2_rbin <= 0;
+	else
+		wq2_rbin <= gray_decode(wq2_rptr);
+end
+
+always @(posedge wr_clk or posedge wr_rst)
+begin
+	if(wr_rst)
+		wr_count <= 0;
+	else
+		wr_count <= wbinnext - wq2_rbin;
 end
 
 endmodule
